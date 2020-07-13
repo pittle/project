6ba57764cc88c630b6b5dd2aabfb8ea82e21e359
zhangjuyu <1021577023@qq.com>
2020-07-13 20:12:44 +0800

    ad search

---
+++ go-to-where/src/components/city/CitySearch.vue
@@ -0,0 +1,35 @@
+<template>
+
+   <div class="search">
+       <input class="search-input" type="text" placeholder="输入城市名或拼音">
+   </div>
+
+</template>
+
+<script>
+
+export default {
+    name:'CitySearch'
+}
+</script>
+
+<style scoped lang='stylus'>
+@import '~styles/varibles.styl'
+
+.search
+    height .72rem
+    background $bgColor
+    padding:0 .1rem
+    .search-input 
+        box-sizing border-box
+        width 100%
+        padding 0 .1rem
+        height .62rem
+        line-height .62rem
+        text-align center
+        border-radius .06rem
+        outline none
+        border none
+        color #666
+    
+</style>
--- go-to-where/src/views/City.vue
+++ go-to-where/src/views/City.vue
@@ -2,16 +2,19 @@
 
     <div>
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 
 </template>
 
 <script>
 import CityHeader from '@/components/city/Header.vue'
+import CitySearch from '@/components/city/CitySearch.vue'
 export default {
     name:'City',
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
